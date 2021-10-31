2b94a7400ef25510f9309a95951ba929a5a68a96
Chaman Bravo <chamanpro9@gmail.com>
2021-10-31 12:22:22 +0545

    mockData

---
+++ frontend/src/context/mockData/data.js
@@ -0,0 +1,122 @@
+export const data = [
+  {
+    id: 1,
+    name: "Aone Hostel",
+    location: "Sankhamul",
+    image: "",
+    price: "10,000",
+    description: "",
+    reviews: [
+      {
+        name: "John Smith",
+        Comment: "very comfortable hostel, best for students",
+      },
+      {
+        name: "Harry Potter",
+        Comment:
+          "this hostel has all the services and suitable environment for students",
+      },
+    ],
+    star: 4.5,
+  },
+  {
+    id: 2,
+    name: "Comfy Hostel",
+    location: "New Baneshwor",
+    image: "",
+    price: "9,000",
+    description: "",
+    reviews: [
+      {
+        name: "John Smith",
+        Comment: "very comfortable hostel, best for students",
+      },
+      {
+        name: "Harry Potter",
+        Comment:
+          "this hostel has all the services and suitable environment for students",
+      },
+    ],
+    star: 4.5,
+  },
+  {
+    id: 3,
+    name: "All Star Hostel",
+    location: "Putalisadak",
+    image: "",
+    price: "12,000",
+    description: "",
+    reviews: [
+      {
+        name: "John Smith",
+        Comment: "very comfortable hostel, best for students",
+      },
+      {
+        name: "Harry Potter",
+        Comment:
+          "this hostel has all the services and suitable environment for students",
+      },
+    ],
+    star: 4.5,
+  },
+  {
+    id: 4,
+    name: "Trilok Hostel",
+    location: "Kritipur",
+    image: "",
+    price: "10,500",
+    description: "",
+    reviews: [
+      {
+        name: "John Smith",
+        Comment: "very comfortable hostel, best for students",
+      },
+      {
+        name: "Harry Potter",
+        Comment:
+          "this hostel has all the services and suitable environment for students",
+      },
+    ],
+    star: 4.5,
+  },
+  {
+    id: 5,
+    name: "Grandy Hostel",
+    location: "Anamnagar",
+    image: "",
+    price: "10,000",
+    description: "",
+    reviews: [
+      {
+        name: "John Smith",
+        Comment: "very comfortable hostel, best for students",
+      },
+      {
+        name: "Harry Potter",
+        Comment:
+          "this hostel has all the services and suitable environment for students",
+      },
+    ],
+    star: 4.5,
+  },
+  {
+    id: 6,
+    name: "Luxy Hostel",
+    location: "Maitighar",
+    image: "",
+    price: "10,000",
+    description: "",
+    reviews: [
+      {
+        name: "John Smith",
+        Comment: "very comfortable hostel, best for students",
+      },
+      {
+        name: "Harry Potter",
+        Comment:
+          "this hostel has all the services and suitable environment for students",
+      },
+    ],
+    star: 4.5,
+  },
+];
