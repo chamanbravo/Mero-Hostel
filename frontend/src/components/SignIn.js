b1510a96bd45bf71cbce25597644ea91c38a1b34
Chaman Bravo <chamanpro9@gmail.com>
2021-11-10 19:35:00 +0545

    C:/Program Files/Git/registeruser

--- frontend/src/components/SignIn.js
+++ frontend/src/components/SignIn.js
@@ -1,25 +1,53 @@
-import React from "react";
-import TextBox from "./TextBox";
-import Button from "./Button";
+import React, { useState } from "react";
+import "./TextBox.scss";
+import axios from "axios";
 
 function SignIn() {
+  const [user, setUser] = useState({ email: "", password: "" });
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  };
+
+  const loginUser = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:4000/login", user, { withCredentials: true })
+      .then(() => {});
+  };
+
   return (
     <div className="register-modal-content">
       <h1>Sign in</h1>
-      <form>
-        <TextBox
-          type="text"
-          placeholder="Email"
-          inputName="Email"
-          name="email"
-        />
-        <TextBox
-          type="password"
-          placeholder="Password"
-          inputName="Password"
-          name="password"
-        />
-        <Button link="#" cName="btn-color" innerText="Sign in" />
+      <form action="" onSubmit={(e) => loginUser(e)}>
+        <div className="input-field">
+          <h4>Email</h4>
+          <input
+            type="text"
+            placeholder="Email"
+            name="email"
+            value={user.email}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="input-field">
+          <h4>Email</h4>
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={user.password}
+            onChange={handleChange}
+          />
+        </div>
+        <button className="btn-color" type="submit">
+          Sign in
+        </button>
       </form>
     </div>
   );
--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -1,37 +1,71 @@
-import React from "react";
-import TextBox from "./TextBox";
-import Button from "./Button";
+import React, { useState } from "react";
+import "./TextBox.scss";
+import axios from "axios";
 
 function SignUp() {
+  const [newUser, setNewUser] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setNewUser({ ...newUser, [name]: value });
+  };
+
+  const registerUser = (e) => {
+    e.preventDefault();
+    axios.post("http://localhost:4000/registerUser", newUser);
+  };
   return (
     <div className="register-modal-content">
       <h1>Sign up</h1>
-      <form>
-        <TextBox
-          type="text"
-          placeholder="First Name"
-          inputName="First Name"
-          name="firstname"
-        />
-        <TextBox
-          type="text"
-          placeholder="Last Name"
-          inputName="Last Name"
-          name="lastname"
-        />
-        <TextBox
-          type="text"
-          placeholder="Email"
-          inputName="Email"
-          name="email"
-        />
-        <TextBox
-          type="password"
-          placeholder="Password"
-          inputName="Password"
-          name="password"
-        />
-        <Button link="#" cName="btn-color" innerText="Sign up" />
+      <form action="" onSubmit={(e) => registerUser(e)}>
+        <div className="input-field">
+          <h4>First Name</h4>
+          <input
+            type="text"
+            placeholder="First Name"
+            value={newUser.firstname}
+            onChange={handleChange}
+            name="firstname"
+          />
+        </div>
+        <div className="input-field">
+          <h4>Last Name</h4>
+          <input
+            type="text"
+            placeholder="Last Name"
+            value={newUser.lastname}
+            onChange={handleChange}
+            name="lastname"
+          />
+        </div>
+        <div className="input-field">
+          <h4>Email</h4>
+          <input
+            type="text"
+            placeholder="Email"
+            value={newUser.email}
+            onChange={handleChange}
+            name="email"
+          />
+        </div>
+        <div className="input-field">
+          <h4>Password</h4>
+          <input
+            type="password"
+            placeholder="Password"
+            value={newUser.password}
+            onChange={handleChange}
+            name="password"
+          />
+        </div>
+        <button className="btn-color" type="submit">
+          Sign up
+        </button>
       </form>
     </div>
   );
