d9076132dad485d55ae4bed88c8f57d490383033
Chaman Bravo <chamanpro9@gmail.com>
2021-11-25 19:49:28 +0545

    host/location form

--- frontend/src/components/HostLocation.js
+++ frontend/src/components/HostLocation.js
@@ -1,5 +1,7 @@
 import React from "react";
 import GoogleMapReact from "google-map-react";
+import "./HostLocation.scss";
+import Button from "./Button";
 
 function HostLocation() {
   const defaultProps = {
@@ -7,16 +9,39 @@ function HostLocation() {
       lat: 27.68883,
       lng: 85.335615,
     },
-    zoom: 11,
+    zoom: 13,
   };
   return (
     // Important! Always set the container height explicitly
-    <div style={{ height: "600px", width: "100%" }}>
-      <GoogleMapReact
-        //   bootstrapURLKeys={{ key: /* YOUR KEY HERE */ }}
-        defaultCenter={defaultProps.center}
-        defaultZoom={defaultProps.zoom}
-      ></GoogleMapReact>
+    <div className="host-location">
+      <div className="mapBox">
+        <GoogleMapReact
+          //   bootstrapURLKeys={{ key: /* YOUR KEY HERE */ }}
+          defaultCenter={defaultProps.center}
+          defaultZoom={defaultProps.zoom}
+        ></GoogleMapReact>
+      </div>
+      <form className="host-location-form">
+        <div>
+          <div className="input-field">
+            <h4>Street</h4>
+            <input type="text" placeholder="Street" name="street" />
+          </div>
+          <div className="input-field">
+            <h4>City</h4>
+            <input type="text" placeholder="City" name="city" />
+          </div>
+          <div className="input-field">
+            <h4>State</h4>
+            <input type="text" placeholder="State" name="state" />
+          </div>
+          <div className="input-field">
+            <h4>Country/Region</h4>
+            <p>Nepal</p>
+          </div>
+        </div>
+        <Button link="host" innerText="Next" cName="btn-black" />
+      </form>
     </div>
   );
 }
