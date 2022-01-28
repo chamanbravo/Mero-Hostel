643f09413b6c5603d6720fcd983e3ce8be67c21a
Chaman Bravo <chamanpro9@gmail.com>
2022-01-28 19:32:58 +0545

    comment input box

--- frontend/src/components/HostelReview.js
+++ frontend/src/components/HostelReview.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./HostelReview.scss";
 import Comments from "./Comments";
 
 function HostelReview({ review }) {
+  const [comment, setComment] = useState("");
+
   return (
     <div className="hostel-review">
-      <h1>Reviews</h1>
+      <h1>{review.length} Reviews</h1>
+      <div className="submit-review">
+        <div className="input-field">
+          <textarea
+            type="text"
+            placeholder="Your Review"
+            name="comment"
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+          />
+        </div>
+        <button className="btn-color" type="submit">
+          Submit
+        </button>
+      </div>
       <div className="all-reviews">
         {review.map((comment, index) => {
           return <Comments {...comment} key={index} />;
